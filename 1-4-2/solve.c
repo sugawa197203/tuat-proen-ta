76193025f7cc091047922f40ad0ecfd4184c33e7
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2026-04-25 17:19:03 +0900

    1-4-2

---
+++ 1-4-2/solve.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+    int i,n,sum;
+
+    printf("nを入力:");
+    scanf("%d",&n);
+    sum=0;
+    
+    for(i=1; i<=n; i++){
+        sum=sum+i; /*和の計算*/
+    }
+    
+    printf("1~%dの和は%d\n",n,sum);
+
+    return 0;
+}
