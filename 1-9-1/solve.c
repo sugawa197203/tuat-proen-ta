ca18802e40a357bce2ab2095ce14f6ccb3218c3a
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2026-06-15 13:10:15 +0900

    1-9-1

---
+++ 1-9-1/solve.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define N 10
+
+void Scalar(int *a, int k){
+    int i;
+    for(i=0; i<N; i++) a[i] = k * a[i];
+}
+
+int main(void){
+    int i;
+    int a[N], k=3;
+    for(i=0; i<N; i++) a[i] = i;
+    Scalar(&a[0], k);
+    for(i=0; i<N; i++) printf("a[%d]は%d\n", i, a[i]);
+    return 0;
+}
