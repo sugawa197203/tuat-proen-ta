545e1a2e933153f106f48f529b7dddec7fb06b60
sugawa197203 <96975428+sugawa197203@users.noreply.github.com>
2026-06-15 13:13:39 +0900

    1-9-1

--- 1-9-1/target.c
+++ 1-9-1/target.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #define N 10
 
-void Scalar(int *a, int k){
+void Scalar(int *a, int k)
+{
     int i;
     for(i=0; i<N; i++) a[i] = k * a[i];
 }
 
-int main(void){
+int main(void)
+{
     int i;
     int a[N], k=3;
     for(i=0; i<N; i++) a[i] = i;
